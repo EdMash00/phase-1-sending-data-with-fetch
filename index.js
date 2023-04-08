70888fdabfaac71d653dd75acf615b131f2cb2c8
EdgarMacharia <m.edgarmacharia@gmail.com>
2023-04-08 09:17:19 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData() {
+
+
+    fetch(`http://localhost:3000/users`,{
+        method : "POST",
+        headers :{
+         "Content-Type": "application/json",
+            Accept: "application/json"
+    },
+        body: JSON.stringify({
+            name:"Steve",
+            email:"steve@steve.com"
+
+    })
+})
+    .then (res => res.json())
+    .then(object => console.log(object))
+
+    .catch(error =>{
+    alert("ERROR")
+    console.log(error.message)
+})
+}
+submitData();
